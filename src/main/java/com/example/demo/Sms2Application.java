1f73865c4054b0cb829b92d7efe39c00b978c4c7
Alexkrish <alexkrish@Alexkrishs-MacBook-Air.local>
2023-10-11 00:05:39 +0530

    configure data

--- src/main/java/com/example/demo/Sms2Application.java
+++ src/main/java/com/example/demo/Sms2Application.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.book.book;
-import com.example.demo.repos.BookRepository;
 
 @SpringBootApplication
 public class Sms2Application {
